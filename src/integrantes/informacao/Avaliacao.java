0c8d331ff6c74a35f4f2566fe13a09fa122b8b93
Igor Rosa <igorrosafilgueiras@gmail.com>
2022-05-10 17:23:19 -0300

    Adding class avaliation

---
+++ src/integrantes/informacao/Avaliacao.java
@@ -0,0 +1,17 @@
+package integrantes.informacao;
+
+public class Avaliacao {
+    private String descricao;
+    private double valor;
+
+    public Avaliacao(String descricao, double valor){
+        this.descricao = descricao;
+        this.valor = valor;
+    }
+
+    @Override
+    public String toString() {
+        
+        return super.toString();
+    }
+}
